1a073bd9c7d2171276de3ef0a96bd847fa8f73ba
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-27 11:37:36 +0200

    useStore

--- src/App.tsx
+++ src/App.tsx
@@ -3,64 +3,19 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Action, type State } from "./types";
 
-const initialState: State = {
-  fromLanguage: "auto",
-  toLanguage: "en",
-  fromText: "",
-  result: "",
-  loading: false,
-};
 
-function reducer(state: State, action: Action) {
-  const { type } = action;
-
-  if (type === "INTERCHANGE_LANGUAGES") {
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage,
-    };
-  }
-
-  if (type === "SET_FROM_LANGUAGE") {
-    return {
-      ...state,
-      fromLanguage: action.payload,
-    };
-  }
-
-  if (type === "SET_TO_LANGUAGE") {
-    return {
-      ...state,
-      toLanguage: action.payload,
-    };
-  }
-
-  if (type === "SET_FROM_TEXT") {
-    return {
-      ...state,
-      loading: true,
-      fromText: action.payload,
-    };
-  }
-
-  if (type === "SET_RESULT") {
-    return {
-      ...state,
-      loading: false,
-      result: action.payload,
-    };
-  }
-
-  return state;
-}
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+ 
 
   return (
     <div className="app">
       <h1>Google Translate</h1>
+      <button
+        onClick={() => dispatch({ type: "SET_FROM_LANGUAGE", payload: "es" })}
+      >
+        Cambiar a Español{" "}
+      </button>
     </div>
   );
 }
