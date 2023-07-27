11c220b5ada803a06ce85e1c6421c06fa2ce1769
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-27 23:19:17 +0200

    interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useStore } from "./hooks/useStore";
 
+import { Container, Row, Col } from "react-bootstrap";
+
 function App() {
   const { fromLanguage, setFromLanguage } = useStore();
 
   return (
-    <div className="app">
+    <Container fluid>
       <h1>Google Translate</h1>
+      <Row>
+        <Col>
+          <h2>From</h2>
+        </Col>
+        <Col>
+          <button>Intercambiar</button>
+        </Col>
+        <Col>
+          <h2>To</h2>
+        </Col>
+      </Row>
       <button onClick={() => setFromLanguage("es")}>Cambiar a Español </button>
       {fromLanguage}
-    </div>
+    </Container>
   );
 }
 
