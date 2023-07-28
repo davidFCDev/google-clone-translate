eadb51e2d409d9a78dda8618470cc35ce22f5943
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-28 22:05:23 +0200

    action tipe

--- src/types.d.ts
+++ src/types.d.ts
@@ -13,9 +13,9 @@ export interface State {
 }
 
 export type Action =
-  | { type: "SET_FROM_LANGUAGE"; payload: string }
+  | { type: "SET_FROM_LANGUAGE"; payload: FromLanguage }
   | { type: "INTERCHANGE_LANGUAGES" }
-  | { type: "SET_TO_LANGUAGE"; payload: string }
+  | { type: "SET_TO_LANGUAGE"; payload: Language }
   | { type: "SET_FROM_TEXT"; payload: string }
   | { type: "SET_RESULT"; payload: string };
 
